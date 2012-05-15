8c654ca9df267bbf4ef7bb65adc733d760f4de2e
Philip Kobernik <philip@bookr.net>
2012-05-14 20:01:11 -0700

    Initial Commit. Anchor Steam Porter.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChoreTime::Application.config.secret_token = 'c6fa597bdb2d7d04e573cc8f7ffadb5e142e54f103a70cd5e1b574c93969da5011aee461840bd3c10c3abe299e4000f3f81cb67993ec3f318b40aa5819dff676'
